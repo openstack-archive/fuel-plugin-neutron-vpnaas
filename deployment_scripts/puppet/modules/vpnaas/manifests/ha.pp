5969efcf4662779f521924252c6ec06cc65a02cb
Sergey Kolekonov <skolekonov@mirantis.com>
2015-08-03 18:13:02 +0300

    Update VPNaaS plugin for 7.0
    - replaced deprecated parameters with actual ones
    - update metadata file for 7.0
    - removed custom packages because upstream mirrors are available for cluster
    - removed q-agent-cleanup.py and cleaned up old code in the OCF file
    
    Change-Id: Id7cf8037ade92bae346164ae14a4a2162ce27cee

--- deployment_scripts/puppet/modules/vpnaas/manifests/ha.pp
+++ deployment_scripts/puppet/modules/vpnaas/manifests/ha.pp
@@ -23,16 +23,7 @@ class vpnaas::ha {
   $neutron_config     = $fuel_settings['quantum_settings']
 
   $debug              = true
-  $syslog             = $fuel_settings['use_syslog'] ? { default=>true }
   $plugin_config      = '/etc/neutron/l3_agent.ini'
-
-  file {'q-agent-cleanup.py':
-    path   => '/usr/bin/q-agent-cleanup.py',
-    mode   => '0755',
-    owner  => root,
-    group  => root,
-    source => 'puppet:///modules/vpnaas/q-agent-cleanup.py',
-  }
 
   file { $vpnaas::params::vpn_agent_ocf_file:
     mode   => '0755',
@@ -66,13 +57,7 @@ class vpnaas::ha {
     ocf_script      => 'ocf-neutron-vpn-agent',
     csr_parameters  => {
       'debug'           => $debug,
-      'syslog'          => $syslog,
       'plugin_config'   => $plugin_config,
-      'os_auth_url'     => "http://${fuel_settings['management_vip']}:35357/v2.0/",
-      'tenant'          => 'services',
-      'username'        => undef,
-      'password'        => $neutron_config['keystone']['admin_password'],
-      'multiple_agents' => $multiple_agents,
     },
     csr_metadata        => $csr_metadata,
     csr_complex_type    => $csr_complex_type,
@@ -88,7 +73,6 @@ class vpnaas::ha {
   }
 
   Exec['remove_p_neutron-l3-agent']                 -> Cluster::Corosync::Cs_service['vpn']
-  File['q-agent-cleanup.py']                        -> Cluster::Corosync::Cs_service['vpn']
   File[$vpnaas::params::vpn_agent_ocf_file]         -> Cluster::Corosync::Cs_service['vpn'] ->
   Cluster::Corosync::Cs_with_service['vpn-and-ovs'] -> Class['vpnaas::common']
 }
